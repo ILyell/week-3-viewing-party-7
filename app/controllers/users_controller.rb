9d9348e783db57bbb4b8a541e2ef2cb03e20619b
Ian Lyell <ian.lyell@yahoo.com>
2023-09-10 13:44:44 -0500

    Add log out feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController <ApplicationController 
+class UsersController < ApplicationController 
   def new 
     @user = User.new()
   end 
@@ -9,7 +9,7 @@ class UsersController <ApplicationController
 
   def create 
     user = User.create(user_params)
-    if user.save
+    if user.save 
       redirect_to user_path(user)
     else  
       flash[:error] = user.errors.full_messages.to_sentence
@@ -17,9 +17,30 @@ class UsersController <ApplicationController
     end 
   end 
 
+  def login_form
+
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.name}"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Bad Credentials"
+      render :login_form
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private 
 
   def user_params 
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
 
+  get '/logout', to: 'users#logout'
   resources :users, only: :show
 end
